9e56ce37a3691a561761c02f8f9559130357b5d3
andybougard <49161648+andybougard@users.noreply.github.com>
2025-04-11 19:14:15 -0500

    moving dockerfile

--- UserManagement/obj/Debug/net8.0/UserManagement.AssemblyInfo.cs
+++ UserManagement/obj/Debug/net8.0/UserManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6e0b7d4c059466e8f9dcd20d977730ee56d1229")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5fec8af3f20cd370f8e8650ff10a6736d29d922")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
