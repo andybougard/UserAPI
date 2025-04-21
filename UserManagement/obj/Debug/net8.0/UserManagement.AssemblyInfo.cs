ca5e050b451e72b11038a3b76a349c0908bc22c9
andybougard <49161648+andybougard@users.noreply.github.com>
2025-04-20 22:54:18 -0500

    ignoring assembly info

--- UserManagement/obj/Debug/net8.0/UserManagement.AssemblyInfo.cs
+++ UserManagement/obj/Debug/net8.0/UserManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5fec8af3f20cd370f8e8650ff10a6736d29d922")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e4f9a736b291e22ae8221dd2ec0b213172668b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
